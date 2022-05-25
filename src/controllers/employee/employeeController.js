7757095a8269266459ba1a403236d246e6c30f02
anisham-cd <104071170+anisham-cd@users.noreply.github.com>
2022-05-25 18:08:50 +0530

    inventory management

---
+++ src/controllers/employee/employeeController.js
@@ -0,0 +1,36 @@
+
+const { response } = require('express');
+let utilsEmployee=require('../../utils/utilsEmployee');
+
+function push(request,response){
+    let body = request.body;
+    body['role']="user";
+    let data=utilsEmployee.push(body)
+    console.log(data)
+    response.send(data)
+}
+function listByName(request,response){
+    let data=utilsEmployee.listByName(request.body)
+    response.send(data)
+}
+function put(request,response){
+    let data=utilsEmployee.modify(request.body)
+    response.send(data)
+}
+function change(request,response){
+    let data=utilsEmployee.update(request.body)
+    response.send(data)
+}
+function getEmployee(request,response){
+    let data=utilsEmployee.get(request)
+    response.send(data)
+}
+    
+
+module.exports={
+    push,
+    listByName,
+    put,
+    change,
+    getEmployee
+}
